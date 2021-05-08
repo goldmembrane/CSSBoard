47cc8f2a1ac681ad3e005c20fdb1f27a482157c8
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-05-08 16:28:57 +0900

    insert elements in login.js file for generate guide lien ; :q

--- cssboard_client/src/component/login.js
+++ cssboard_client/src/component/login.js
@@ -3,7 +3,10 @@ import React from 'react'
 const login = () => {
     return (
         <>
-            <div>Login</div>
+            <div>
+                <button>login</button>
+                <a href = 'http://localhost:3000'>forgot ID/Password</a>
+            </div>
         </>
     )
 }
